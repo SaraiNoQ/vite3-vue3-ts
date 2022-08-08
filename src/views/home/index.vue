ace325315e4d4b5527a7d7c99a73d1351b2f4d00
SaraiNoQ <2994498974@qq.com>
2022-08-08 21:13:19 +0800

    n

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -6,5 +6,8 @@
 <script setup lang="ts">
 import Hero from "./components/Hero.vue";
 import StoreTest from "./components/StoreTest.vue";
+const a = () => {
+  return "b";
+};
 </script>
 <style scoped></style>
